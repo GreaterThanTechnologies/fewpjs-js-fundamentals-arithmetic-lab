4511f160515d2629432bc69fd6d5fd3c92a1e38a
James Hyatt <76410700+GreaterThanTechnologies@users.noreply.github.com>
2021-04-21 20:31:06 -0400

    commit tree 04568d9d24474625c24ce90a297b4876035bab27

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let num3 = 4
+let num4 = 6
+let mod = num3 % num4
+let max = Math.max(1,20,4,7)
+let random = Math.floor(Math.random() * 10) + 1
+let multiply = num1 * num2
